30f27a354ffe13320a9557665d4157f1ed43f925
Daubas Emiliano <emilianodaubas@gmail.com>
2018-02-01 10:13:07 -0300

    V1

---
+++ Assets/scripts/Rotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    
+    public float delay;
+    float timecounter = 0;
+    float radio = 6;
+
+    void Start()
+    {
+        timecounter = delay;
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(new Vector3(30, 30, 30) * Time.deltaTime);
+
+        timecounter += Time.deltaTime;
+        //float y = 0.1f / Time.deltaTime;
+        float y = 2;
+        float x = Mathf.Cos(timecounter) * radio;
+        float z = Mathf.Sin(timecounter) * radio;
+        transform.position = new Vector3(x, y, z);
+    }
+}
---
+++ Assets/scripts/player_cont.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_cont : MonoBehaviour {
+
+    public float speed;
+    public Text textCount;
+    public Text textWin;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        textWin.text = "";
+    }
+   	
+	void FixedUpdate () {
+        float hMovement = Input.GetAxis("Horizontal");
+        float vMovement = Input.GetAxis("Vertical");
+
+        //JUMP!!
+        Vector3 movement = new Vector3(hMovement * speed, 0.0f, vMovement * speed);
+        if (Input.GetKeyDown(KeyCode.Space) & transform.position.y == 0.5f)
+        {
+            movement = new Vector3(hMovement * speed, 300.0f, vMovement * speed);
+        }
+        else
+        {
+            movement = new Vector3(hMovement * speed, 0.0f, vMovement * speed);
+        };
+        rb.AddForce(movement);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up")) {
+
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        textCount.text = "Score: " + count.ToString();
+        if (count == 6)
+        {
+            textWin.text = "WIN!!!";
+        }
+
+    }
+}
